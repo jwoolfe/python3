cfcb697408913e69d3c1e80ca83bac5c5de9f885
jwoolfe <jen@jenwoolfe.com>
2020-02-21 17:16:22 -0800

    Add Wizard class and got rid of global LOCATION.

--- wiz7.py
+++ wiz7.py
@@ -2,7 +2,6 @@
 import sys
 import readline
 
-LOCATION = "forest"
 SKILL = 0
 GOLD = 0
 BOOKS = 1
@@ -21,17 +20,39 @@ GOLD = 10
 #  apply literate programming (comments)
 #  test out completion algorithm in >>> (IDE)
 
+class Wizard:
+    def __init__(self, location="forest"):
+        self.location = location
+
+        self.locations = {
+            "forest" : "You travel to the forest where " \
+                "you can forage for mushrooms and relax.",
+            "tower"  : "You travel to your tower where there is peace and quiet.",
+            "village" : "You travel to your village where " \
+                "you can work, sell goods and shop.",
+            "black rock city" : "You travel to the playa )'(.",
+            }
+
+    def travel(self, location):
+        if location not in self.locations: 
+            print(f'''{location} is not a place in this land.''')
+        elif location == self.location:
+            print(f'''You are already in {location}.''')
+        else:
+            self.location = location
+            print(f"{self.locations[location]}")
 
 def main():
     readline.set_auto_history(True)
     readline.set_completer(completion)
     wiz_help()
+    wiz = Wizard()
 
     try:
         while True:
             task = input("> ")
             task = task.strip().lower()
-            request(task)
+            request(wiz, task)
     except (KeyboardInterrupt, EOFError):
         bye()
 
@@ -62,11 +83,10 @@ def completion(text, state):
         return None
     return matches[state]
 
-def request(task):
+def request(wiz, task):
     global EGO
     global GOLD
     global BOOKS
-    global LOCATION
     global MUSHROOMS
     global POTIONS
     global SKILL
@@ -74,17 +94,17 @@ def request(task):
 
     if task in ["quit", "q", "Q"]:
         bye()
-    elif task == LOCATION:
-        print(f"You are at the {LOCATION}.")
+    elif task in wiz.locations:
+        wiz.travel(task)       
     elif task in ["location", "where"]:
-        if LOCATION is None:
+        if wiz.location is None:
             print('''You are nowhere. Where would you like to go?''')
         else:
-            print(f"You are at the {LOCATION}.")
+            print(f"You are at the {wiz.location}.")
     elif task in ["skill level", "skill", "skills"]:
         print(f'''You are skill level {SKILL}.''' )
     elif task == "study":
-        if LOCATION == "tower":
+        if wiz.location == "tower":
             if SKILL < BOOKS:
                 SKILL = SKILL + 1
                 STRESS += STRESS
@@ -93,31 +113,20 @@ def request(task):
             else:
                 print('''You have no more new books to read. ''')
         else:
-            print(f'''You cannot study in the {LOCATION}.''')
-    elif task == "tower":
-        LOCATION = "tower"
-        print('''You travel to your tower where there is quiet.''')
-    elif task == "village":
-        LOCATION = "village"
-        print ('''You travel to your village ''' 
-         '''where you can work, sell goods and shop.''')
-    elif task == "forest":
-        LOCATION = "forest"
-        print('''You travel to the forest '''   
-            '''where you can forage for mushrooms and relax. ''')
+            print(f'''You cannot study in the {wiz.location}.''')
     elif task == "relax":
-        if LOCATION == "forest":
+        if wiz.location == "forest":
             STRESS -= 1
         else:
-            print(f'''You can't relax in {LOCATION}. ''')
+            print(f'''You can't relax in {wiz.location}. ''')
     elif task == "forage":
-        if LOCATION == "forest":
+        if wiz.location == "forest":
             MUSHROOMS += 1
             print(f'''You now have {MUSHROOMS} mushrooms. ''')
         else:
-            print(f'''There are no mushrooms in the {LOCATION}. ''')
+            print(f'''There are no mushrooms in the {wiz.location}. ''')
     elif task == "work":
-        if LOCATION == "village":
+        if wiz.location == "village":
             if SKILL > 0:
                 GOLD += SKILL
                 STRESS += STRESS
@@ -125,9 +134,9 @@ def request(task):
             else:
                 print(f'''You can't work without any skills.''')
         else:
-            print(f'''There is no work in the {LOCATION}.''')
+            print(f'''There is no work in the {wiz.location}.''')
     elif task == "shop":
-        if LOCATION == "village":
+        if wiz.location == "village":
             if GOLD == 0:
                 print(f'''You have {GOLD} gold. You must work to earn gold.''')
             else:
@@ -136,10 +145,7 @@ def request(task):
                 STRESS += 1
                 print(f'''You now have {BOOKS} books and {GOLD} gold.''')
         else:
-            print(f'''You can't shop in the {LOCATION}.''')
-    elif task in ["black rock", "playa", "brc", "black rock city"]:
-        LOCATION = "black rock city"
-        print('''You travel to the playa )'(.  ''')
+            print(f'''You can't shop in the {wiz.location}.''')
     elif task == "gold":
         print(f'''You have {GOLD} gold.''')
     elif task == "purse":
@@ -153,7 +159,7 @@ def request(task):
             SKILL: {SKILL}
             STRESS: {STRESS} ''')
     elif task == "brew":
-        if LOCATION == "tower":
+        if wiz.location == "tower":
             if MUSHROOMS == 0:
                 print(f'''You can't brew potions without mushrooms. ''')
             else:
@@ -162,16 +168,16 @@ def request(task):
                 STRESS += 1
                 print(f'''You have now have {POTIONS} potions.''')
         else:
-            print(f'''You cannot brew potions in the {LOCATION}. ''')
+            print(f'''You cannot brew potions in the {wiz.location}. ''')
     elif task == "sell":
-        if LOCATION == "village":
+        if wiz.location == "village":
             if POTIONS > 0:
                 GOLD += GOLD
                 print(f'''You now have {GOLD} gold.''')
             else:
                 print(f'''You have no brewed potions to sell.''')
         else:
-                print(f'''You can't sell goods in the {LOCATION}.''')
+                print(f'''You can't sell goods in the {wiz.location}.''')
     elif task in ["help", "h", "?"]:
         wiz_help()
     else:
