d06f38b55dc577c39d05d32fb2c4f9375e437344
jwoolfe <jen@jenwoolfe.com>
2020-05-08 18:58:30 -0700

    More exercises, zigzag and heart.

--- exercises.py
+++ exercises.py
@@ -5,7 +5,7 @@
 def sum_list(items):
     total = 0
     for i in items:
-        total += i
+        total += i 
     return total
 
 # multiply all items in a list
@@ -28,9 +28,36 @@ def return_smallest(items):
         return smallest
 
 
-numbers = [8, 17, 121, 65, 93, 44, 5, 20, 4, 1]
+numbers = [8, 17, 121, 65, 93, 44, 5, 20, 4, 1 ]
 
 print('Sum of all numbers is: ', sum_list(numbers))
 print('All numbers multiplied is: ', multiply_list(numbers))
 print('Largest number is: ', return_largest(numbers))
 print('Smallest number is: ', return_smallest(numbers))
+
+
+print('\n')
+# Function that calculates the collatz sequence
+
+def collatz(num):
+    num = int(num)
+    print(num)
+    if num % 2 == 0:     # even
+        calc = num // 2
+    else:                   # odd
+        calc = (3 * num) + 1
+    return calc
+
+number = input("Enter a number: ")
+number = collatz(int(number))
+while number != 1:
+    number = collatz(number)
+print(number)
+
+
+
+
+
+
+
+
