087077e5c7c2f8f9a4299d2839d3e07e0fee235f
jwoolfe <jen@jenwoolfe.com>
2020-09-21 16:54:19 -0700

    Added 6 functions for handling users.

--- wiz/game/auth.py
+++ wiz/game/auth.py
@@ -5,6 +5,56 @@ import os.path
 
 DEFAULT_FILENAME = "/srv/users.auth"
 
+def login(name, password):
+    user = getuser(name)
+    if user is not None:
+        salt = user['salt']
+        oldhash = user['oldhash']
+        if checkpass(password, salt, oldhash):
+            return True
+    return False
+
+def adduser(name, password):
+    user = getuser(name)
+    if user is not None:
+        salt = getsalt()
+        oldhash = hashsalt(password, salt)
+        user = {
+                'username' : name,
+                'salt': salt,
+                'oldhash': oldhash,
+                }
+        users = load()
+        users.append(user)
+        save(users)
+        return True
+    return False
+
+def getuser(name):
+    users = load()
+    for user in users:
+        if name == user['username']:
+            return user
+    return None
+
+def checkpass(password, salt, oldhash):
+    newhash = hashsalt(password, salt)
+    if oldhash == newhash:
+        return True
+    return False
+
+def changepass(name, oldpass, newpass):
+    if login(name, oldpass):
+        # create newusers list
+        # get oldusers list
+        # for each user in oldusers:
+        #   if this user is OUR user, skip it
+        #   otherwise add it to newusers
+        # save(newusers)
+        # adduser(name, newpass)
+        return True
+    return False
+
 def getsalt():
     return uuid.uuid4().hex
 
@@ -28,11 +78,16 @@ def load():
             user = {
                 'username' : username,
                 'salt': salt,
-                'password': password,
+                'oldhash': password,
             }
             users.append(user)
     return users
 
-def save():
-    pass
-            
+def save(users):
+    with open(DEFAULT_FILENAME, 'w') as f:
+        for user in users:
+            username = user['username']
+            salt = user['salt']
+            password = user['oldhash']
+            line = f'{username}:{salt}:{password}\n'
+            f.write(line)
